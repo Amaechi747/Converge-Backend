e48c03d276e30fbd6cbdc7304897c750fd1fe277
amoses1-mufin <amaechimoses8@gmail.com>
2024-08-17 09:54:36 +0100

    Create user

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/auth', (req: Request, res: Response) => {
+
+    
   res.send('Hello, TypeScript with Express!');
 });
 
--- src/models/user.ts
+++ src/models/user.ts
@@ -4,6 +4,11 @@ interface User {
   id: number;
   name: string;
   email: string;
+  password: string;
+  passwordChanged: boolean;
+  phoneNum: string;
+  team: string;
+  pix: string;
 }
 
 export const createUserTable = async () => {
@@ -11,15 +16,27 @@ export const createUserTable = async () => {
   await db.exec(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    email TEXT
+    email TEXT,
+    passwordChanged BOOLEAN,
+    password TEXT,
+    phoneNum TEXT,
+    team TEXT,
+    pix TEXT
   )`);
 };
 
-export const createUser = async (name: string, email: string) => {
+export const createUser = async (name: string, 
+    email: string, 
+    passwordChanged: boolean, 
+    password: string, 
+    phoneNum: string, 
+    team: string, 
+    pix: string
+    ) => {
   const db = await dbPromise;
   const result = await db.run(
-    `INSERT INTO users (name, email) VALUES (?, ?)`,
-    [name, email]
+    `INSERT INTO users (name, email, passwordChanged, password, phoneNum, team, pix) VALUES (?, ?)`,
+    [name, email, passwordChanged, password, phoneNum, team, pix]
   );
   return result;
 };
